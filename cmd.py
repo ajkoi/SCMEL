106e3a1fa91c26b4bbb07d656b69f2cc78623e33
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-10-11 22:29:13 +0200

    testouillation

--- cmd.py
+++ cmd.py
@@ -5,3 +5,7 @@ import classements
 class MyShell(cmd.cmd):
     intro = "Welcome to the SCMEL shell"
     prompt = "$"
+
+    def hello(self, arg):
+        name = arg.strip() or "stranger"
+        print(name)
