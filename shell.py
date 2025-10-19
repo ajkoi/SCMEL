85a4ba530d485791368b079c29fb88710d786079
ajkoi <felix.yiou.alias.yeou@gmail.com>
2025-10-19 16:49:00 +0200

    fonction de fin de partie

--- shell.py
+++ shell.py
@@ -9,24 +9,59 @@ class MyShell(cmd.Cmd):
     def do_nouveau_joueur(self, line):
         """Args: nom, prénom, crée un nouveau joueur"""
         args = line.split(" ")
-        if len(args) > 2:
+        if len(args) != 2:
             print("Seulement le nom et le prénom")
         else:
             if classements.recup_joueur(*args):
                 print("Ce joueur existe déjà.")
             else:
                 classements.nouveau_joueur(*args)
-                print(f"{*args} à été rajouté aux joueurs.")
+                print(f"{args[0]} {args[1]} à été rajouté aux joueurs.")
         pass
 
     def do_fin_de_partie(self, line):
+        """
+        Arguments : nom1, prenom1, nom2, prénom2, issue, format
+        issue : 1 ou 0.5 ou 0, du point de vue du joueur 1 (1 est une victoire, 0.5 est un match nul et 0 est une défaite).
+        format: bullet ou rapide ou blitz.
+        """
+        args = line.split(" ")
+        if len(args) != 6:
+            print("Mauvais nombre d'arguments")
+        elif args[4] not in ["1", "0.5", "0"]:
+            print("Mauvais argument pour l'issue du match")
+        elif args[5] not in ["bullet", "blitz", "rapide"]:
+            print("Mauvais argument pour le format du match")
+        else:
+            args[4] = int(
+                args[4]
+            )  # avoir un int, on vérifie maintenant car on est sur que c'est un integer
+            joueur1 = classements.recup_joueur(*args[0:2])
+            joueur2 = classements.recup_joueur(*args[2:4])
+            if joueur1 == "":
+                print("Le premier joueur n'est pas enregistré")
+                return 0
+            if joueur2 == "":
+                print("Le deuxième joueur n'est pas enregistré.")
+                return 0
+
+            classements.fin_de_game(joueur1, joueur2, *args[4:])
+            print("La fin de match à été enregistré.")
+            print(
+                f"Nouvel elo de {args[0]} {args[1]} : {classements.recup_joueur(*args[0:2])[args[5]]}."
+            )
+            print(
+                f"Nouvel elo de {args[2]} {args[3]} : {classements.recup_joueur(*args[2:4])[args[5]]}."
+            )
+
+    def do_classement(self, line):
         pass
 
     def do_exit(self, line):
         """
         Sortir du shell.
         """
-        print("Au revoir")
+        print("Au revoir.")
         return True
 
 
